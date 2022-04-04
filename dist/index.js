3b73e7cee33e406c964678662ca63721c662f4a7
Dominic Rico Gómez <hello@coderocket.me>
2022-04-04 01:00:02 +0100

    make the action great again

--- dist/index.js
+++ dist/index.js
@@ -8342,7 +8342,7 @@ const getReposToDeploy = async () => {
 (async () => {
   try {
     const r = await getReposToDeploy()
-    core.setOutput('matrix', r)
+    core.setOutput('matrix', { include: r })
   } catch (error) {
     core.setFailed(error.message)
   }
