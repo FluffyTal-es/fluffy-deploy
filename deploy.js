09a596676b1a0f07a8ce0699725a4e4322e45fa4
Dominic Rico Gómez <hello@coderocket.me>
2022-04-04 01:15:49 +0100

    make the action great again

--- deploy.js
+++ deploy.js
@@ -5,8 +5,15 @@ const octokit = new Octokit({
   auth: process.env.DEPLOY_TOKEN || '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'
 })
 
+const determineFolder = (name) => {
+  if (name.indexOf('job') !== -1) return '[jobs]'
+  else if (name.indexOf('dev') !== -1) return '[dev]'
+  else if (name.indexOf('configs') !== -1) return '[configs]'
+  else return '[fluffy]'
+}
+
 const getReposToDeploy = async () => {
-  const reposToDeploy = core.getInput('repos')
+  const reposToDeploy = 'all' //core.getInput('repos')
 
   if (reposToDeploy === 'all') {
     const repos = await octokit.rest.repos
@@ -15,7 +22,10 @@ const getReposToDeploy = async () => {
         type: 'private',
       })
 
-    return repos.data.map(repo => { name: repo.full_name })
+    return repos.data.map(repo => ({ 
+      name: repo.full_name,
+      path: `resources/${determineFolder(repo.name)}/`
+    }))
   }
 
   return [{ name: `FluffyTal-es/${reposToDeploy}` }]
