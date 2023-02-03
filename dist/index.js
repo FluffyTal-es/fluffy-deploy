35c527293efdc7b679a9383985c5657b67d26467
derkleinlord <dennis.luecking@yahoo.de>
2023-02-03 15:48:19 +0100

    cars

--- dist/index.js
+++ dist/index.js
@@ -8354,6 +8354,8 @@ const getRemoteDestinationFolder = (name) => {
   else if (name.indexOf('fluffy-mlos') !== -1) return '[fluffy-mlos]'
   else if (name.indexOf('fluffy-peds') !== -1) return '[fluffy-peds]'
   else if (name.indexOf('fluffy-moddedcars') !== -1) return '[fluffy-moddedcars]'
+  else if (name.indexOf('fluffy-car-mdfd') !== -1) return '[fluffy-moddedcars]'
+  else if (name.indexOf('fluffy-car-event') !== -1) return '[fluffy-moddedcars]'
   else if (name == 'fluffy-car-addons')  return '[fluffy]/[fluffy-car-addons]'
   else if (name == 'fluffy-car-pdsd')  return '[fluffy]/[fluffy-car-pdsd]'
   else if (name == 'fluffy-car-import-am') return '[fluffy]/[fluffy-car-import-am]'
