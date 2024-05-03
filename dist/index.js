d6a2f9e398169642059fde119530519855506718
Dominic Rico Gómez <hello@coderocket.me>
2024-05-03 15:00:55 +0200

    update for fd

--- dist/index.js
+++ dist/index.js
@@ -8357,6 +8357,7 @@ const getRemoteDestinationFolder = (name) => {
   else if (name == 'fluffy-car-addons') return '[fluffy-moddedcars]/[fluffy-car-addons]'
   else if (name == 'fluffy-car-pdmd') return '[fluffy-moddedcars]/[fluffy-car-pdmd]'
   else if (name == 'fluffy-car-event') return `[fluffy-moddedcars]/${name}`
+  else if (name == 'fluffy-cars-fd') return `[fluffy-moddedcars]/[${name}]`
   else if (name == 'fluffy-cars-pd-cc') return `[fluffy-moddedcars]/${name}`
   else if (name == 'fluffy-cars-md-dl') return `[fluffy-moddedcars]/${name}`
   else if (name == 'illenium-appearance') return `[spawn]/${name}`
